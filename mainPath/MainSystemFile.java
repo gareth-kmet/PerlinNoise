98ea664fe3c230489ed051508ac4af112e30bb1b
gareth-kmet <gareth.kmet@gmail.com>
2022-12-07 18:48:04 -0700

    Testing remote access

--- mainPath/MainSystemFile.java
+++ mainPath/MainSystemFile.java
@@ -1,13 +1,6 @@
 package mainPath;
-import util.Vector2f;
 
 public class MainSystemFile {
     public static void main(String[] args) {
-        Vector2f a = Vector2f.e1();
-        Vector2f b = Vector2f.e2();
-        
-        System.out.println(Vector2f.add(a, b));
-        System.out.println(Vector2f.scale(a, 2));
-        System.out.println(Vector2f.dot(a, b));
     }
 }

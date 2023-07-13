66f7d466d816f4717e92e00d5d6c8e69a48f8d20
gareth-kmet <89107606+gareth-kmet@users.noreply.github.com>
2023-07-12 18:43:56 -0600

    Updated C++ Project
    Works in eclipse now

--- C++/src/PerlinNoise.cpp
+++ C++/src/PerlinNoise.cpp
@@ -3,13 +3,14 @@
 // Author      : 
 // Version     :
 // Copyright   : Your copyright notice
-// Description : Hello World in C, Ansi-style
+// Description : Hello World in C++, Ansi-style
 //============================================================================
 
-#include <stdio.h>
-#include <stdlib.h>
+#include <iostream>
+using namespace std;
 
-int main(void) {
-	puts("Hello World!!!");
-	return EXIT_SUCCESS;
+int main() {
+	cout << "Hello World!!!" << endl; // prints Hello World!!!
+	cout << "This is great" << endl;
+	return 0;
 }
